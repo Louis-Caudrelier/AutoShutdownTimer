47aa6c66e952572a34634e57e05dd05f365a7dba
Pandidoux <32765824+Louis-Caudrelier@users.noreply.github.com>
2019-01-30 22:47:21 +0100

    Restructuration
    Support de machine "headlless"
    Arrêt plus simple

--- AutoShutdownTimer.bat
+++ AutoShutdownTimer.bat
@@ -1,25 +1,31 @@
 @echo off
 chcp 65001 > nul
 COLOR 0a
+mode con lines=20 cols=100
 rem ______________________________________________________________________
+:annuler
+color 0a
 cls
-echo Anulation préventive des extinctions programmées :
+echo #####################################################################
+echo #               Annulation de l'extinction programmée                #
+echo #####################################################################
 shutdown -a
 echo.
+goto defTime
 rem ______________________________________________________________________
 :defTime
 echo Eteindre l'ordinateur dans combien de temps ?
 echo.
 rem ----------------------
-set /p varheures=Nombre d'heures ?
+set /p varheures=Nombre d'heures: 
 if not %varheures% LEQ 48 goto timeError
 if %varheures% LSS 0 goto timeError
 echo.
-set /p varminutes=Nombre de minutes ?
+set /p varminutes=Nombre de minutes: 
 if not %varminutes% LEQ 60 goto timeError
 if %varminutes% LSS 0 goto timeError
 echo.
-set /p varsecondes=Nombre de secondes ?
+set /p varsecondes=Nombre de secondes: 
 if not %varsecondes% LEQ 60 goto timeError
 if %varsecondes% LSS 0 goto timeError
 rem ----------------------
@@ -31,8 +37,8 @@ rem ----------------------
 :validation
 echo %totalTime% secondes avant extinction.
 echo.
-set /p c=Continuer ? (O/N)
-if /i %c%==o goto wait
+set /p c=Continuer ? (O/N) : 
+if /i %c%==o goto eteindre
 if /i %c%==n goto fin
 goto continuError
 rem ______________________________________________________________________
@@ -51,18 +57,21 @@ cls
 goto validation
 rem ______________________________________________________________________
 :wait
+color 0c
+cls
 echo #####################################################################
 echo # JE SUIS PROGRAMME POUR M'ETEINDRE AUTOMATIQUEMENT. NE PAS TOUCHER #
+echo #    ----- Appuyez sur une touche pour annuler l'extinction -----   #
 echo #####################################################################
-color 0e
-timeout -t %totalTime% /nobreak
+timeout -t %totalTime%
+rem /nobreak
+goto annuler
 rem ______________________________________________________________________
-color 0c
-shutdown -s -t 60
-pause
-goto fin
+:eteindre
+shutdown -s -t %totalTime%
+goto wait
 rem ______________________________________________________________________
 :fin
 echo.
 echo ########### Fin du Programme ###########
-Pause
+pause
